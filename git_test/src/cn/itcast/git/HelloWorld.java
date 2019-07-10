5096a1d688c525f137a1389b761a26796e34df79
wangshi <1203477624@qq.com>
2019-07-10 19:23:00 +0800

    此次更新功能： 1.Hello

---
+++ git_test/src/cn/itcast/git/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("哇哇哇哇");
+        System.out.println("哇哇哇哇");
+        System.out.println("哇哇哇哇");
+        System.out.println("哇哇哇哇");
+    }
+}
